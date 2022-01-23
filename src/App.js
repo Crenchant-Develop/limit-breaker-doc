218ce6fc0027fff0c68e9d82fd2941b6892cb695
DESKTOP-5CN50DO\Administrator <levelhoon@gmail.com>
2022-01-24 05:05:52 +0900

    numberCircle위에 글씨 가운데 정렬

--- src/App.js
+++ src/App.js
@@ -6,127 +6,129 @@ import ReactFullpage from "@fullpage/react-fullpage";
 import logo from './questLogo.png';
 import circle from './circle.svg';
 import numberCircle from './numberCircle.svg';
-import bar from './bar.svg';
 import LayoutView from "./LayoutView";
 
 import './App.css';
 
-
-
 const anchors = ["firstPage", "secondPage", "thirdPage", "fourthPage"];
 
 function MySection(props) {
-  return (
-    <div className="section">
-      <h3>{props.children}</h3>
-    </div>
-  );
+    return (
+        <div className="section">
+            <h3>{props.children}</h3>
+        </div>
+    );
 }
 
 const Sections = () => (
-  <ReactFullpage
-    anchors={anchors}
-    navigation
-    navigationTooltips={anchors}
-    onLeave={(origin, destination, direction) => {
-      console.log("onLeave event", { origin, destination, direction });
-    }}
-    render={({ state, fullpageApi }) => {
-      console.log("render prop change", state, fullpageApi); // eslint-disable-line no-console
+    <ReactFullpage
+        anchors={anchors}
+        navigation
+        navigationTooltips={anchors}
+        onLeave={(origin, destination, direction) => {
+            console.log("onLeave event", { origin, destination, direction });
+        }}
+        render={({ state, fullpageApi }) => {
+            console.log("render prop change", state, fullpageApi); // eslint-disable-line no-console
 
-      return (
-        <div>
-          <MySection>
-            <MainView />
-          </MySection>
+            return (
+                <div>
+                    <MySection>
+                        <MainView />
+                    </MySection>
 
-          <MySection>
-            <MainIntro />
-          </MySection>
+                    <MySection>
+                        <MainIntro />
+                    </MySection>
 
-          <MySection>
-            <MianContext number = "1" />
-          </MySection>
+                    <MySection>
+                        <MianContext number="1" />
+                    </MySection>
 
-          <MySection>
-            <LayoutView title="fourthPage" content="내용4" />
-          </MySection>
-        </div>
-      );
-    }}
-  />
+                    <MySection>
+                        <LayoutView title="fourthPage" content="내용4" />
+                    </MySection>
+                </div>
+            );
+        }}
+    />
 );
 
 function MainIntro() {
-  return (
-    <div className="Main">
-      <header className="Main-header">
-        <img src={circle} className="Main-circle" alt="circle" />
-        <div className="Center-text">목차</div>
-      </header>
-    </div>
-  );
+    return (
+        <div className="Main">
+            <header className="Main-header">
+                <img src={circle} className="Main-circle" alt="circle" />
+                <div className="Center-text">목차</div>
+            </header>
+        </div>
+    );
 }
 
 function MianContext(props) {
 
-  const titleImage = () => {
-    return (
-      <div>
-        <div class="flex-container">
-          <div className="Number" >
-            <img src={numberCircle} className="Number-circle" alt="numberCircle" />
-            <div style={{background: url(numberCircle)}} >
-              {props.number}
+    const titleImage = () => {
+        return (
+            <div>
+                <div class="flex-container">
+                    <div className="Number" >
+                        <div className="Number-circle" alt="numberCircle"
+                            style = {{
+                                backgroundImage: `url( ${numberCircle} )`
+                            }}
+                        >
+                            <div className="Number-text">
+                                {props.number}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Bar"/>
+                </div>
             </div>
-          </div>
-          <div className="Bar">
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 
-  const title = (image) => {
+    const title = (image) => {
+        return (
+            <>
+                {titleImage()}
+                제목
+            </>
+        );
+    }
+
+    const contentText = () => {
+        return (
+            <>
+                테스트
+            </>
+        );
+    }
+
+
     return (
-      <>
-        {titleImage()}
-        제목
-      </>
+        <>
+            <LayoutView title={title()} content={contentText()} />
+        </>
     );
-  }
-
-  const contentText = () => {
-    return (
-      <>
-        테스트
-      </>
-    );
-  }
-
-
-  return (
-    <>
-      <LayoutView title={title()} content={contentText()} />
-    </>
-  );
 }
 
 function MainView() {
-  return (
-    <div className="Main">
-      <header className="Main-header">
-        <img src={logo} className="Main-logo" alt="logo" />
-        <p>
-          Limit Breaker 기획서
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="Main">
+            <header className="Main-header">
+                <img src={logo} className="Main-logo" alt="logo" />
+                <p>
+                    Limit Breaker 기획서
+                </p>
+            </header>
+        </div>
+    );
 }
 
 function App() {
-  return (<Sections />);
+    return (<Sections />);
 }
 
 export default App;
