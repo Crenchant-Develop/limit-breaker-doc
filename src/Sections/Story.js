0e737a0600cd4cb1387d3bcb744db6443a7999f6
DESKTOP-6JECQ4U\Administrator <blackscore@naver.com>
2022-01-31 18:23:33 +0900

    스토리보드 큰틀짬

--- src/Sections/Story.js
+++ src/Sections/Story.js
@@ -1,20 +1,24 @@
 import React from "react";
-import mainPage from '../img/mainPage.png';
-import mainPage2 from '../img/mainPage2.png';
+import StoryBoard from '../img/001.png';
+import StoryBoard2 from '../img/002.png';
+import StoryBoard3 from '../img/003.png';
 
 export function Story() {
     return (
         <>
             <div className="Main-text-center">
-                게임이 시작하기 전에 들르게 되는 화면이다.<br />
-                스테이지들을 선택할 수 있고, 그동안 얻은 보상으로 오브를 강화할 수 있다.
+                둥둥 떠다니는 거대한 침대 위에 있고 온통 행복한 기억들과 꿈들로<br />
+                이루어진 <mark>드리미아 왕국.</mark> 그곳에는 <mark>트라움</mark>이라는 이름의 한 왕자가 있었다.
             </div>
             <div className="Image-box-wrapper">
                 <div className="Image-box">
-                    <img src={mainPage} alt="Main Page 1" />
+                    <img src={StoryBoard} alt="Storyboard 1" />
                 </div>
                 <div className="Image-box">
-                    <img src={mainPage2} alt="Main Page 2" />
+                    <img src={StoryBoard2} alt="Storyboard 2" />
+                </div>
+                <div className="Image-box">
+                    <img src={StoryBoard3} alt="Storyboard 3" />
                 </div>
             </div>
         </>
