3edaccab2d68cdae8681dea3f4539a087ae370bc
DESKTOP-5CN50DO\Administrator <levelhoon@gmail.com>
2022-02-03 08:03:56 +0900

    스플레쉬 로고 페이드아웃 구현

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverf
 import { Section } from "./Section";
 import MainView from "./Sections/MainView";
 
+import './Fade.css';
 import './App.css';
 import './Main.css';
 import './Title.css';
@@ -14,23 +15,21 @@ import './Image.css';
 import Sections from "./Sections";
 
 
-
-
 //이제 스플레쉬 인트로 Logo화면이 구현되었습니다.
 //state 변수인 isIntro가 3초뒤 true로 변하면서 본문이 보이게 변경했습니다.
 function App() {
-    // const [isIntro, setIntro] = useState(true);
-    // if (isIntro) {
-    //     setTimeout(() => {
-    //         setIntro(false);
-    //     },
-    //         3000
-    //     )
+    const [isIntro, setIntro] = useState(true);
+    if (isIntro) {
+        setTimeout(() => {
+            setIntro(false);
+        },
+            3000
+        )
 
-    //     return (
-    //         <MainView />
-    //     );
-    // }
+        return (
+            <MainView />
+        );
+    }
     return <Sections />
 }
 
--- src/Sections/MainView.js
+++ src/Sections/MainView.js
@@ -3,8 +3,8 @@ import logo from '../img/questLogo.png';
 
 export default function MainView() {
     return (
-        <div className="Main">
-            <header className="Main-header">
+        <div className="Main ">
+            <header className="Main-header fade-out-box">
                 <img src={logo} className="Main-logo" alt="logo" />
             </header>
         </div>
