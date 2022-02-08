21664685e193baac614a391e745c2e43749f9a4d
DESKTOP-6JECQ4U\Administrator <blackscore@naver.com>
2022-02-08 13:05:18 +0900

    퀘스트 로고를 이미지 호스팅으로 돌림

--- src/Sections/MainView.js
+++ src/Sections/MainView.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from '../img/questLogo.png';
 
 export default function MainView() {
     return (
         <div className="Main fade-out-box">
             <header className="Main-header">
-                <img src={logo} className="Main-logo" alt="logo" />
+                <img src="https://i.ibb.co/S6T3wQ9/quest-Logo.png" className="Main-logo" alt="quest-Logo" border="0" />
             </header>
         </div>
     );
